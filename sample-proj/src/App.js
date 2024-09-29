0fb8b17b0e4596b672deaa138be1d9914ff83952
skader14 <saifkader14@gmail.com>
2024-09-29 17:43:09 -0500

    shut up

--- sample-proj/src/App.js
+++ sample-proj/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. MAIN VERSION 1 - Shama
+          Edit <code>src/App.js</code> and save to reload. MAIN VERSION 1 - Saif
         </p>
         <a
           className="App-link"
